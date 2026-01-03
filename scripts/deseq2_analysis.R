ddec57fc5b1cddcedfba1a21a2bf26b605bc578f
Dr. Zaiba Hasan Khan <68143089+zaibahasankhan@users.noreply.github.com>
2026-01-04 01:47:35 +0530

    Add DESeq2 analysis script placeholder

---
+++ scripts/deseq2_analysis.R
@@ -0,0 +1,7 @@
+# DESeq2 RNA-seq differential expression analysis
+# Author: Zaiba Hasan Khan
+
+# This script will include:
+# - Loading count data
+# - DESeq2 analysis
+# - Visualization (volcano plot, heatmap)
